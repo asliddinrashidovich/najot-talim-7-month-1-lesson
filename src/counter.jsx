4e48600ae4dc53540bd8edc4074c157ce92ab120
Asliddin <asliddinrashidovich7@gmail.com>
2025-04-12 15:46:58 +0500

    f

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,8 +1,11 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './reducers/counter'
+import { decrement, decrementByAmount, increment, incrementByAmount } from './reducers/counter'
+import { useState } from 'react'
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value)
+  const [num, setNum] = useState()
+  const [num2, setNum2] = useState()
   const dispatch = useDispatch()
 
   return (
@@ -20,7 +23,25 @@ export function Counter() {
           onClick={() => dispatch(decrement())}
         >
           Decrement
-        </button>
+        </button><br />
+        <div className='my-[10px]'>
+            <input placeholder='Number' type="number" className='border' value={num} onChange={(e) => setNum(e.target.value)}/>
+            <button className='p-[5px] bg-[#555] text-[#fff] mx-[10px]'
+            aria-label="Decrement value"
+            onClick={() => dispatch(incrementByAmount(Number(num)))}
+            >
+            Increment by this number
+            </button>
+        </div>
+        <div className='my-[10px]'>
+            <input placeholder='Number' type="number" className='border' value={num2} onChange={(e) => setNum2(e.target.value)}/>
+            <button className='p-[5px] bg-[#555] text-[#fff] mx-[10px]'
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrementByAmount(Number(num2)))}
+            >
+            Decrement by this number
+            </button>
+        </div>
       </div>
     </div>
   )
